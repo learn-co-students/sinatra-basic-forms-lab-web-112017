733ac2c46245c1542109bef0619d252a111034b5
Flatiron School <leslie.hsu@flatironschool.com>
2017-12-13 17:33:41 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,28 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get "/" do
+
+    erb :index
+  end
+
+  get "/new" do
+
+    erb :create_puppy
+  end
+
+#  get "/create_puppy" do
+#
+#    erb :create_puppy
+#  end
+
+
+
+  post "/create_puppy" do
+    @new_pup = Puppy.new(params[:name], params[:breed], params[:age])
+    erb :display_puppy
+  end
+
+
+
 end

728951a003e492aad0f0071f5e57d6164cfc8da7
Flatiron School <leslie.hsu@flatironschool.com>
2017-12-13 17:45:46 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,21 +2,13 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get "/" do
-
-    erb :index
+  get "/" do   #=>a page that has a form (i.e. index.erb)
+    erb :index  #=> if you click the link, it will post to '/new'"
   end
 
   get "/new" do
-
-    erb :create_puppy
+    erb :create_puppy  #=>just renders the create_puppy form; this form posts to :create_puppy
   end
-
-#  get "/create_puppy" do
-#
-#    erb :create_puppy
-#  end
-
 
 
   post "/create_puppy" do
